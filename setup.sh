299795182af6e91a510d35a51b23f94157728771
khannover-fmedien <138600593+khannover-fmedien@users.noreply.github.com>
2024-11-13 15:15:48 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do
 
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -18,7 +18,7 @@ echo \
 sudo apt-get update
 
 # Install docker packages
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Verify docker installation
 sudo docker run hello-world
